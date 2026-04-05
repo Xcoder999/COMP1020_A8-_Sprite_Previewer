258dd004a66e9dc10f0a16960c9ce55b7e82b406
Isaac 阮卓民 Yuen Cheuk Man Yuen <isaac.yuen2@gmail.com>
2026-04-04 23:57:28 -0600

    fps feature added

--- SpritePreviewer.py
+++ SpritePreviewer.py
@@ -1,3 +1,4 @@
+#COMP1020_A8-_Sprite_Previewer
 import math
 
 from PyQt6.QtGui import *
@@ -23,9 +24,33 @@ class SpritePreview(QMainWindow):
         # This loads the provided sprite and would need to be changed for your own.
         self.num_frames = 21
         self.frames = load_sprite('spriteImages',self.num_frames)
+        self.current_frame = 0
+        self.fps = 1
+
+        self.label_chr = QLabel()
+        self.label_chr.setPixmap(self.frames[0])
+        self.label_chr.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label_chr.setScaledContents(True)
+        self.label_chr.setSizePolicy(
+            QSizePolicy.Policy.Expanding,
+            QSizePolicy.Policy.Expanding
+        )
 
         # Add any other instance variables needed to track information as the program
         # runs here
+        self.label_slider = QLabel(f"Frame per second: {self.fps}")
+        self.label_slider.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.slider = QSlider(Qt.Orientation.Vertical)
+        self.slider.setMinimum(1)
+        self.slider.setMaximum(30)
+        self.slider.setValue(self.fps)
+        self.slider.setInvertedAppearance(True)
+        self.slider.valueChanged.connect(self.on_fps_changed)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.next_frame)
+        self.timer.start(1000 // self.fps)
 
         # Make the GUI in the setupUI method
         self.setupUI()
@@ -39,8 +64,25 @@ class SpritePreview(QMainWindow):
         # the other components of the program.
         # Create needed connections between the UI components and slot methods
         # you define in this class.
+        slider_layout = QVBoxLayout()
+        slider_layout.addWidget(self.label_slider)
+        slider_layout.addWidget(self.slider)
 
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(self.label_chr)
+        main_layout.addLayout(slider_layout)
+
+        frame.setLayout(main_layout)
         self.setCentralWidget(frame)
+
+    def next_frame(self):
+        self.current_frame = (self.current_frame + 1) % self.num_frames
+        self.label_chr.setPixmap(self.frames[self.current_frame])
+
+    def on_fps_changed(self, value):
+        self.fps = value
+        self.label_slider.setText(f"Frame per second: {value}")
+        self.timer.start(1000 // value)
 
 
     # You will need methods in the class to act as slots to connect to signals
